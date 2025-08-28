79c4d4db3f5305ff33bad9dd1f9f82997e1c05e0
Serhii Kermas <serhii.kermas@gmail.com>
2025-08-28 14:17:14 +0300

    Solution

---
+++ INSTRUCTION.md
@@ -0,0 +1,9 @@
+http://localhost:30008/
+kubectl get pods
+kubectl exec todoapp-6dbd8c574d-2lg7v -it -- sh 
+ls
+cd configs
+ls
+cd ../secrets
+ls
+cat SECRET_KEY
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+kubectl apply -f ./.infrastructure/namespace.yml
+kubectl apply -f ./.infrastructure/pv.yml
+kubectl apply -f ./.infrastructure/pvc.yml -n todoapp
+kubectl apply -f ./.infrastructure/clusterIp.yml -n todoapp
+kubectl apply -f ./.infrastructure/nodeport.yml -n todoapp
+kubectl apply -f ./.infrastructure/secret.yml -n todoapp
+kubectl apply -f ./.infrastructure/configMap.yml -n todoapp
+kubectl apply -f ./.infrastructure/hpa.yml -n todoapp
+kubectl apply -f ./.infrastructure/deployment.yml -n todoapp
